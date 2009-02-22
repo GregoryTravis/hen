48148ab59efb3b47747ef4bdab3d91a5272e2f6d
Gregory Travis <greg.m.travis@gmail.com>
2009-02-22 11:56:35 -0500

    Hadn't declared ref stuff for charp.

--- ref.h
+++ ref.h
@@ -2,5 +2,6 @@
 #define DEKKIE(_type) _type* create_##_type##_ref(_type i); _type read_##_type##_ref(_type* ip); void write_##_type##_ref(_type* ip, _type i); void destroy_##_type##_ref(_type* ip); 
 DEKKIE(int)
 DEKKIE(float)
+DEKKIE(charp)
 
 void* create_null_ref(void);
