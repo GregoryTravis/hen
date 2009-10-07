f3d3819b5f5acec9ce43a715a05ea536ffc0db2e
Gregory Travis <greg.m.travis@gmail.com>
2009-10-07 13:25:45 -0400

    dumping

--- vm.c
+++ vm.c
@@ -6,21 +6,41 @@
 
 #include "yeah.h"
 
+void dump(yeah* y);
+void dump1(yeah* y);
+
+void dump1_list(yeah* y) {
+  match(y) {
+    Symbol(txt) { printf(" . "); dump1(y); printf(")"); }
+    Pair(car, cdr) { printf(" "); dump1(car); dump1_list(cdr); }
+    Nil() { printf(")"); }
+    end;
+  }
+}
+
+void dump1(yeah* y) {
+  match (y) {
+    Symbol(txt) { printf("%s", txt); }
+    Pair(car, cdr) { printf("("); dump1(car); dump1_list(cdr); }
+    Nil() { printf("()"); }
+    end;
+  }
+}
+
+void dump(yeah* y) {
+  dump1(y);
+  printf("\n");
+}
+
 int main(int argc, char** argv) {
   printf("vm!\n");
 
-  yeah* y = mksymbol("asdf");
-  y = mkpair(mksymbol("asdf"), mksymbol("awer"));
-
-  match(y) {
-    Symbol(joe) {
-      printf("sym!\n");
-    }
-    Pair(a, d) {
-      printf("pair!\n");
-    }
-    end;
-  }
+  dump(mksymbol("asdf"));
+  dump(mkpair(mksymbol("asdf"), mksymbol("awer")));
+  dump(mkpair(mksymbol("asdf"), mknil()));
+  dump(mkpair(mksymbol("asdf"), mkpair(mksymbol("joe"), mknil())));
+  dump(mkpair(mksymbol("asdf"), mkpair(mksymbol("joe"), mkpair(mksymbol("lap"), mknil()))));
+  dump(mknil());
 
   return 0;
 }
