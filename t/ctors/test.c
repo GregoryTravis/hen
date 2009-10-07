09c2050cbb205e73e6983542e2c814b73257c92c
Gregory Travis <greg.m.travis@gmail.com>
2009-10-07 13:40:17 -0400

    test.ss test-input.ss Getting in the way of proper ut.

---
+++ t/ctors/test.c
@@ -0,0 +1,48 @@
+// $Id$
+
+#include <stdio.h>
+
+#include "yeah.h"
+
+void dump(yeah* y);
+void dump1(yeah* y);
+
+void dump1_list(yeah* y) {
+  match(y) {
+    Symbol(txt) { printf(" . "); dump1(y); printf(")"); }
+    Pair(car, cdr) { printf(" "); dump1(car); dump1_list(cdr); }
+    Nil() { printf(")"); }
+    end;
+  }
+}
+
+void dump1(yeah* y) {
+  match (y) {
+    Symbol(txt) { printf("%s", txt); }
+    Pair(car, cdr) { printf("("); dump1(car); dump1_list(cdr); }
+    Nil() { printf("()"); }
+    end;
+  }
+}
+
+void dump(yeah* y) {
+  dump1(y);
+  printf("\n");
+}
+
+int main(int argc, char** argv) {
+  printf("vm!\n");
+
+  dump(mksymbol("asdf"));
+  dump(mkpair(mksymbol("asdf"), mksymbol("awer")));
+  dump(mkpair(mksymbol("asdf"), mknil()));
+  dump(mkpair(mksymbol("asdf"), mkpair(mksymbol("joe"), mknil())));
+  dump(mkpair(mksymbol("asdf"), mkpair(mksymbol("joe"), mkpair(mksymbol("lap"), mknil()))));
+  dump(mkpair(mkpair(mksymbol("asdf"), mksymbol("qwer")), mksymbol("zxcv")));
+  dump(mknil());
+
+  printf("%d %d %d\n", issymbol(mksymbol("asdf")), ispair(mkpair(mksymbol("asdf"), mksymbol("awer"))), isnil(mknil()));
+  printf("%d %d %d\n", ispair(mksymbol("asdf")), isnil(mkpair(mksymbol("asdf"), mksymbol("awer"))), issymbol(mknil()));
+
+  return 0;
+}
