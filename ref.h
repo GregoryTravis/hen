bf6dfb7ed481005cb662146de6cd8e6e1738f0df
Gregory Travis <greg.m.travis@gmail.com>
2009-11-21 17:21:32 -0500

    Copy in rigg and a working sample, ref.h/c.

---
+++ ref.h
@@ -0,0 +1,15 @@
+#include <math.h>
+
+#ifndef _ref_h_
+#define _ref_h_
+
+typedef char* charp;
+
+#define DEKKIE(_type) _type* create_##_type##_ref(_type i); _type read_##_type##_ref(_type* ip); void write_##_type##_ref(_type* ip, _type i); void destroy_##_type##_ref(_type* ip); 
+DEKKIE(int)
+DEKKIE(float)
+DEKKIE(charp)
+
+void* create_null_ref(void);
+
+#endif  // _ref_h_
